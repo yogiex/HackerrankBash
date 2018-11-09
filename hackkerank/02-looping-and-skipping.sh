eb34194d24ba44e39b9bd71b5acdd084031e58b7
Priyagung Yogi <31413884+yogiex@users.noreply.github.com>
2018-11-09 09:08:41 +0700

    Update and rename loopingskipp.sh to 02-looping-and-skipping.sh

---
+++ hackkerank/02-looping-and-skipping.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+clear
+for i in {1..99} #perulangan loop EOP pada angka 99 
+do
+	if(( $i % 2 == 1)) #mengecek kondisi ganjil atau bukan
+	then #lalu
+		echo $i #output dari variabel i
+	fi #eop
+done
+#menggunakan perulangan sekaligus menggunakan kondisi if statement
